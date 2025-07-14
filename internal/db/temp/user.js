ef5ca1941911f1ccb2b386b4b6b8dd358518d63b
GTAJIT <mukhujeegtajit@gmail.com>
2025-07-15 02:21:54 +0530

    feat: main page, createEvent page, editEvent page created; fix: login page and temp: user, event db created

---
+++ internal/db/temp/user.js
@@ -0,0 +1,21 @@
+// Demo users for login/signup logic
+export const users = [
+    {
+        userId: 1,
+        userName: "gtajit1",
+        userEmail: "one@one.one",
+        userPassword: "ONE.one@2005",
+    },
+    {
+        userId: 2,
+        userName: "jit",
+        userEmail: "jitmukherjee.dev@gmail.com",
+        userPassword: "one@2005",
+    },
+    {
+        userId: 3,
+        userName: "gtajit",
+        userEmail: "one@one.old",
+        userPassword: "jit@2005",
+    },
+];
